6cb9a95d92107ae986135c016ff74087c61aff86
gkrishnans <gokulakrishnanaji@gmail.com>
2022-03-22 14:50:30 +0530

    solutions to the assignment 1 basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FailureComponent } from './failure-component/failure-component.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { SuccessComponentComponent } from './success-component/success-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    SuccessComponentComponent,
+    FailureComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/failure-component/failure-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-failure-component',
+    templateUrl:'./failure-component.component.html',
+    styleUrls:['failure-component.component.css']
+})
+export class FailureComponent{
+
+}
